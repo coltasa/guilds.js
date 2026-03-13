afc12ad15d290c9bbd767cdd8a180387692498e4
coltasa <notspammail@icloud.com>
2026-03-13 21:38:14 +0200

    feat: continued typings & event data

--- packages/guilds.js/src/types/index.ts
+++ packages/guilds.js/src/types/index.ts
@@ -1,6 +1,6 @@
 import type { Gateway } from "@/classes/gateway"
 import type { RESTManager } from "@/classes/rest-manager"
-import type { APIUser } from "@/api"
+import type { APIGuildMember, APIMessage, APISnowflake, APIUser } from "@/api"
 
 export interface ClientProps {
     gateway: Gateway
@@ -20,11 +20,29 @@ export type GatewayEventMap = {
     ]
 }
 
+/** @see https://docs.discord.com/developers/events/gateway-events */
 export interface GatewayEvents {
+    MESSAGE_CREATE: APIMessage & {
+        guild_id?: APISnowflake
+        member?: Partial<APIGuildMember>
+        mentions: (APIUser & { member?: Partial<APIGuildMember> | null })[]
+    }
+
+    MESSAGE_DELETE: {
+        channel_id: APISnowflake
+        guild_id?: APISnowflake
+        id: APISnowflake
+    }
+
+    MESSAGE_UPDATE: APIMessage & { tts: false }
+
     READY: {
         user: APIUser
         session_id: string
     }
+
+    WS_DEBUG: string
+    WS_ERROR: string
 }
 
 export interface GatewayProps {

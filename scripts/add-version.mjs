05c9a7e9718c189915518ea2247e4091391936c9
windowsnashville <notspammail@icloud.com>
2026-03-19 10:49:43 +0200

    ci(add-version): check for dist/ dir

--- scripts/add-version.mjs
+++ scripts/add-version.mjs
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
-import { appendFile, readFile } from "node:fs/promises"
+import { access, appendFile, readFile, constants } from "node:fs/promises"
 import path from "node:path"
 import url from "node:url"
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+const dist = path.resolve(__dirname, "..", "packages", "guilds.js", "dist")
+
+try {
+    await access(dist, constants.F_OK)
+} catch {
+    console.error("The dist/ directory is missing.")
+    process.exit(1)
+}
+
 const { version } = JSON.parse(
     await readFile(
         path.resolve(__dirname, "..", "packages", "guilds.js", "package.json"),
@@ -12,14 +21,12 @@ const { version } = JSON.parse(
     )
 )
 
-await appendFile(
-    path.resolve(__dirname, "..", "packages", "guilds.js", "dist", "index.cjs"),
-    `\nexports.version = "${version}";\n`
-)
-
-await appendFile(
-    path.resolve(__dirname, "..", "packages", "guilds.js", "dist", "index.d.cts"),
-    `\nexport declare const version = "${version}";\n`
-)
+await Promise.all([
+    appendFile(path.resolve(dist, "index.cjs"), `\nexports.version = "${version}";\n`),
+    appendFile(
+        path.resolve(dist, "index.d.cts"),
+        `\nexport declare const version = "${version}";\n`
+    ),
+])
 
 console.log(`Version string: ${version}`)

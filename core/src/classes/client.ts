3a937d677f269b2fadb9fda8ef8fff17420f20a3
andrewdku <notspammail@icloud.com>
2026-03-31 22:35:32 +0200

    chore(client): throw TypeError for invalid props

--- core/src/classes/client.ts
+++ core/src/classes/client.ts
@@ -8,6 +8,23 @@ export class Client {
     public intents: number
 
     public constructor(props: ClientProps) {
+        if (!props || typeof props !== "object") {
+            throw new TypeError("Invalid client props provided")
+        }
+
+        if (!props.token || typeof props.token !== "string") {
+            throw new TypeError("Invalid token provided")
+        }
+
+        if (
+            !props.intents ||
+            (typeof props.intents !== "number" &&
+                typeof props.intents !== "string" &&
+                !Array.isArray(props.intents))
+        ) {
+            throw new TypeError("Invalid intents provided")
+        }
+
         this.#token = parseToken(props.token)
         this.intents = parseIntents(props.intents)
     }

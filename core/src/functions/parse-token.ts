dead844f06e19edbedb4954e1a3c5b03b6942a52
andrewdku <notspammail@icloud.com>
2026-03-31 22:20:58 +0200

    refactor(parse-token): throw TypeError for invalid token

--- core/src/functions/parse-token.ts
+++ core/src/functions/parse-token.ts
@@ -1,4 +1,8 @@
 export function parseToken(token: string): string {
+    if (!token || typeof token !== "string") {
+        throw new TypeError("Invalid token provided")
+    }
+
     if (token.startsWith("Bot ")) {
         return token
     } else {
